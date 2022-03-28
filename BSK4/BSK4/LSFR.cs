589324afe7153d51d315b440842668b41da91268
konrad121970 <92802840+konrad121970@users.noreply.github.com>
2022-03-28 12:15:04 +0200

    Add files via upload

---
+++ BSK4/BSK4/LSFR.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BSK4
+{
+    public static class LSFR
+    {
+        public static void Init(string data)
+        {
+            _Bits = new List<int>();
+            _Polynomial = new List<int>();
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                if(data[i] == '1') {
+                    _Polynomial.Add(i);
+                }
+            }
+
+            var x = Convert.ToString(DateTime.Now.ToBinary(), 2);
+            x = x.Substring(x.Length - data.Length);
+
+            for(int i = 0; i < x.Length; i++)
+            {
+                _Bits.Add(int.Parse(x[i].ToString()));
+            }
+
+            Console.WriteLine("Wartosc poczatkowa: {0}", x);
+        }
+
+        public static int GenerateBit()
+        {
+            var result = _Bits[_Bits.Count - 1];
+            var last_bit = _Bits[_Polynomial[_Polynomial.Count - 1]];
+
+            for(int i = _Polynomial.Count - 1; i > 0; i--)
+            {
+                int bit1 = _Polynomial[i - 1];
+
+                if(_Bits[bit1] == last_bit)
+                {
+                    last_bit = 0;
+                }
+                else
+                {
+                    last_bit = 1;
+                }
+            }
+
+            for(int i = _Bits.Count - 1; i > 0; i--)
+            {
+                _Bits[i] = _Bits[i - 1];
+            }
+
+            _Bits[0] = last_bit;
+            return result;
+        }
+
+        private static List<int> _Bits;
+        private static List<int> _Polynomial;
+    }
+}
---
+++ BSK4/BSK4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BSK4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LSFR.Init("100101");
+
+            for(int i = 0; i < Math.Pow(2, 9) - 1; i++)
+            {
+                var bit = LSFR.GenerateBit();
+                Console.Write(bit);
+            }
+        }
+    }
+}
